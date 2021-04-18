17d465c1443d882e555ccd99a050608bb9f494c0
刘冲 <liuchong@vmovier.com>
2021-04-18 17:20:09 +0800

    feat 组件化工程demo

---
+++ ModulesDemo/ModulesDemo/Modules/ModulesList/Models/ListModel.h
@@ -0,0 +1,20 @@
+//
+//  ListModel.h
+//  ModulesDemo
+//
+//  Created by 刘冲 on 2021/1/16.
+//  Copyright © 2021 lc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ListModel : NSObject
+
+@property (nonatomic, copy)   NSString *ID;
+@property (nonatomic, copy)   NSString *count;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ModulesDemo/ModulesDemo/Modules/ModulesList/Models/ListModel.m
@@ -0,0 +1,13 @@
+//
+//  ListModel.m
+//  ModulesDemo
+//
+//  Created by 刘冲 on 2021/1/16.
+//  Copyright © 2021 lc. All rights reserved.
+//
+
+#import "ListModel.h"
+
+@implementation ListModel
+
+@end
